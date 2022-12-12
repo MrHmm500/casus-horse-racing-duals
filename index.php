a1075cb9ee3c5ac1f0814ab26cb363cb7bc42b57
ruby <ruby.van.soelen@omines.com>
2022-12-12 16:05:34 +0100

    Refactoring: optimized code, internal functionality ussage

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 $jsonTestCases = file_get_contents('testcases.json');
 $testCases = json_decode($jsonTestCases);
-foreach($testCases as $caseName => $caseData) {
+foreach ($testCases as $caseName => $caseData) {
     echo "-----------------------------------<br />";
     echo $caseName . ' wordt getest<br />';
     echo 'expected output: <br />';
@@ -17,20 +17,17 @@ foreach($testCases as $caseName => $caseData) {
 
 function extractOutputFromInput($horses)
 {
-    $minDiff = 9999999999999999999999999999999999999;
+    $minDiff = PHP_INT_MAX;
     sort($horses);
     $horses = array_values($horses);
 
     for ($i = 0; $i < count($horses) - 1; $i++)
     {
-        $difference = (int)$horses[$i] - (int)$horses[$i + 1];
-        if ($difference < 0) {
-            $difference *= -1;
-        }
+        $difference = abs((int)$horses[$i] - (int)$horses[$i + 1]);
         if ($difference < $minDiff) {
             $minDiff = $difference;
         }
     }
 
-    echo("$minDiff\n");
+    echo("$minDiff <br />");
 }
